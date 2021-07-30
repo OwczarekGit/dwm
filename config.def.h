81170382e894a49b1713c245495fcd23897438da
OwczarekGit <null>
2021-07-30 12:51:11 +0200

    Moved files and calculator to sxhkd

--- config.def.h
+++ config.def.h
@@ -71,16 +71,12 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[]   = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]    = { "sakura", NULL };
 static const char *browser[]    = { "firefox", NULL };
-static const char *files[]      = { "pcmanfm", NULL };
-static const char *calculator[] = { "galculator", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ ALTKEY,                       XK_space,  spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_f,      spawn,          {.v = browser } },
-	{ MODKEY,                       XK_n,      spawn,          {.v = files } },
-	{ MODKEY,                       XK_c,      spawn,          {.v = calculator } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
