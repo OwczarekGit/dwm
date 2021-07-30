a066c851c6153a8f1381fa563da79995cc8dcd8e
OwczarekGit <null>
2021-07-30 12:30:02 +0200

    Moved colors to config.h

---
+++ colors.h
@@ -0,0 +1,5 @@
+static const char col_gray1[]       = "#002459";
+static const char col_gray2[]       = "#12437E";
+static const char col_gray3[]       = "#F1DFDD";
+static const char col_gray4[]       = "#E0D4C8";
+static const char col_cyan[]        = "#135B96";
