0df499cc597221ec6bea0ef8036635c8d38a9bd2
OwczarekGit <null>
2021-07-31 14:40:54 +0200

    Changed colors

--- colors.h
+++ colors.h
@@ -1,5 +1,5 @@
 static const char col_gray1[]       = "#212320"; // Workspace/Tray BG
-static const char col_gray2[]       = "#203C52"; // IDK
+static const char col_gray2[]       = "#203C52"; // Unfocused window border
 static const char col_gray3[]       = "#FFFFFF"; // Workspace squares
 static const char col_gray4[]       = "#E1E3E0"; // Text
-static const char col_cyan[]        = "#203C52"; // Window name BG
+static const char col_cyan[]        = "#1693CF"; // Window name BG/Focused window frame
