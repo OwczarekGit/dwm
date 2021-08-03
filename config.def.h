5e3a3e56326389febb3ccecb5359dde777f10aa3
OwczarekGit <32412218+OwczarekGit@users.noreply.github.com>
2021-08-03 03:23:41 +0200

    Added scratchpad, its not perfect but i will work on it.

--- config.def.h
+++ config.def.h
@@ -73,8 +73,8 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[]   = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]    = { "sakura", NULL };
-static const char *browser[]    = { "firefox", NULL };
+static const char *termcmd[]    = { "alacritty", NULL };
+static const char *browser[]    = { "google-chrome-stable", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -118,6 +118,9 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY,                       XK_s, scratchpad_show,     {0} },
+	{ MODKEY|ShiftMask,             XK_s, scratchpad_hide,     {0} },
+	{ MODKEY,                       XK_d,scratchpad_remove,    {0} },
 };
 
 /* button definitions */
